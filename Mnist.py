479edea6a75f80063b2d4ce4ff17b920cd61d2bc
Sourav Debnath <souravdebnath.ic@gmail.com>
2021-06-15 21:39:44 +0600

    Add files via upload

---
+++ Mnist.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+import tensorflow as tf
+
+mnist=tf.keras.datasets.mnist
+
+(x_train,y_train),(x_test,y_test)= mnist.load_data()
+
+x_train=tf.keras.utils.normalize(x_train, axis=1)
+x_test=tf.keras.utils.normalize(x_test, axis=1)
+
+model=tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(128,activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(128,activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(10,activation=tf.nn.softmax))
+
+model.compile(optimizer= 'adam', loss= 'sparse_categorical_crossentropy', metrics=['accuracy'] )
+model.fit(x_train,y_train,epochs=5)
+
+
+# In[10]:
+
+
+val_loss, val_acc = model.evaluate(x_test, y_test)
+print(val_loss,val_acc)
+
+
+# In[4]:
+
+
+import matplotlib.pyplot as plt
+
+plt.imshow(x_train[0], cmap=plt.cm.binary)
+plt.show()
+
+
+# In[12]:
+
+
+prediction= model.predict(x_test)
+
+import numpy as np
+print(np.argmax(prediction[30]))
+
+
+# In[14]:
+
+
+plt.imshow(x_test[30], cmap=plt.cm.binary)
+plt.show()
+
+
+# In[ ]:
+
+
+
+
